b1f700923b638264baf5f497c5d31fa32510d4c4
maclops <lol@grr.la>
2018-10-29 15:21:39 -0300

    Incluido logica de calculos

--- app/src/main/java/calculadora_de_gorjeta/maclops/com/calculadoradegorjeta/MainActivity.java
+++ app/src/main/java/calculadora_de_gorjeta/maclops/com/calculadoradegorjeta/MainActivity.java
@@ -2,12 +2,60 @@ package calculadora_de_gorjeta.maclops.com.calculadoradegorjeta;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editValor;
+    private TextView textPorcentagem;
+    private TextView textGorjeta;
+    private TextView textTotal;
+    private SeekBar seekGorjeta;
+
+    private double porcentagem = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editValor = findViewById(R.id.editValor);
+        textPorcentagem = findViewById(R.id.textPorcentagem);
+        textGorjeta = findViewById(R.id.textGorjeta);
+        textTotal = findViewById(R.id.textTotal);
+        seekGorjeta = findViewById(R.id.seekGorjeta);
+
+        seekGorjeta.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                porcentagem = seekBar.getProgress();
+                textPorcentagem.setText(Math.round(porcentagem)+"%");
+                calcular();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
+
+    public void calcular() {
+        double valorDigitado = Double.parseDouble(editValor.getText().toString());
+
+        double gorjeta = valorDigitado * (porcentagem/100);
+        double total = gorjeta + valorDigitado;
+
+        textGorjeta.setText("R$ "+ Math.round(gorjeta));
+        textTotal.setText("R$ "+total);
+    }
+
 }
