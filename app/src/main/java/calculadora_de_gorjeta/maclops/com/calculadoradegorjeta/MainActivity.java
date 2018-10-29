dc66d1d04ea09a08d7379c5cf275efa2ad4635db
maclops <lol@grr.la>
2018-10-29 14:57:10 -0300

    Criado layout da aplicacao

---
+++ app/src/main/java/calculadora_de_gorjeta/maclops/com/calculadoradegorjeta/MainActivity.java
@@ -0,0 +1,13 @@
+package calculadora_de_gorjeta.maclops.com.calculadoradegorjeta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
